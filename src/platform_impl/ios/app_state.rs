613df20598a53d821b0e6a94a16ecdbcf7cdd73f
lsy969999 <lsy969999@gmail.com>
2024-08-15 21:47:09 +0900

    remove log

--- src/platform_impl/ios/app_state.rs
+++ src/platform_impl/ios/app_state.rs
@@ -543,12 +543,12 @@ pub(crate) fn handle_nonuser_events<I: IntoIterator<Item = EventWrapper>>(
         match wrapper {
             EventWrapper::StaticEvent(event) => {
                 if !processing_redraws && event.is_redraw() {
-                    tracing::info!("processing `RedrawRequested` during the main event loop");
+                    // tracing::info!("processing `RedrawRequested` during the main event loop");
                 } else if processing_redraws && !event.is_redraw() {
-                    tracing::warn!(
-                        "processing non `RedrawRequested` event after the main event loop: {:#?}",
-                        event
-                    );
+                    // tracing::warn!(
+                    //     "processing non `RedrawRequested` event after the main event loop: {:#?}",
+                    //     event
+                    // );
                 }
                 handler.handle_event(event)
             },
@@ -588,13 +588,13 @@ pub(crate) fn handle_nonuser_events<I: IntoIterator<Item = EventWrapper>>(
             match wrapper {
                 EventWrapper::StaticEvent(event) => {
                     if !processing_redraws && event.is_redraw() {
-                        tracing::info!("processing `RedrawRequested` during the main event loop");
+                        // tracing::info!("processing `RedrawRequested` during the main event loop");
                     } else if processing_redraws && !event.is_redraw() {
-                        tracing::warn!(
-                            "processing non-`RedrawRequested` event after the main event loop: \
-                             {:#?}",
-                            event
-                        );
+                        // tracing::warn!(
+                        //     "processing non-`RedrawRequested` event after the main event loop: \
+                        //      {:#?}",
+                        //     event
+                        // );
                     }
                     handler.handle_event(event)
                 },
